f87e887cf30d224eb7bd940b749bc0039dd7b511
chrystianruan <chrystianr37@gmail.com>
2024-12-16 13:46:24 -0300

    fix: corrigindo erro de build

--- src/main/java/com/api/shopan/entities/Category.java
+++ src/main/java/com/api/shopan/entities/Category.java
@@ -24,7 +24,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 
     public CategoryDTO parseToDTO() {
--- src/main/java/com/api/shopan/repositories/CategoryRepository.java
+++ src/main/java/com/api/shopan/repositories/CategoryRepository.java
@@ -3,11 +3,13 @@ package com.api.shopan.repositories;
 import com.api.shopan.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     @Query("select count(category) > 1 from Category category where category.name ilike concat('%', :name, '%')")
     boolean existsByName(String name);
 
-    @Query("select count(category) > 1 from Category category where category.name ilike concat('%', :name, '%') and category <> ?2")
+    @Query("select count(category) > 1 from Category category where category.name ilike concat('%', :name, '%') and category <> :category")
     boolean existsByNameAndCurrentObjectDifferent(String name, Category category);
 }
